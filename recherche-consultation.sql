df8b7ef7e08d209eca4d9469afa1210c82b9ffb9
pierre-geeraert <pierre.geeraert@viacesi.fr>
2017-03-31 15:27:44 +0200

    Create recherche-consultation.sql

---
+++ recherche-consultation.sql
@@ -0,0 +1,22 @@
+
+Les potions ou les onguents réalisables avec un ingrédient précis
+
+
+
+
+Les couples (potions, onguents) utilisant la  liste d’ingrédients
+
+
+
+
+Les potions réalisables avec La liste des potions classée par température de préparation
+
+
+
+La comparaison entre le prix de vente d’une potion et le total du prix des ingrédients (et diluant) nécessaires à sa réalisation
+
+
+
+
+
+Le nombre moyen d’ingrédients utilisés pour fabriquer une potion (ou onguent)
