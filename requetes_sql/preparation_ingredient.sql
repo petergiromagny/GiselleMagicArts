e095c4fa6cbe13fa579196b6a8868107448e1cba
AlexandreLec <alexandre.lecomte@viacesi.fr>
2017-04-03 16:26:05 +0200

    prepa ingre

--- requetes_sql/preparation_ingredient.sql
+++ requetes_sql/preparation_ingredient.sql
@@ -11,9 +11,13 @@ DELIMITER |
 CREATE PROCEDURE preparation_ingredient (IN ingredient varchar(255))
 BEGIN
 
-SELECT *
-FROM se_compose_prepa, Produit
-WHERE se_compose_prepa.IDproduit = Produit.IDproduit;
+SELECT DISTINCT Produit.NomProduit AS 'Nom Preparation'
+FROM se_compose_prepa INNER JOIN Produit ON se_compose_prepa.IDproduitFinal = Produit.IDproduit
+WHERE se_compose_prepa.IDproduitFinal IN (
+  SELECT IDproduitFinal
+  FROM se_compose_prepa INNER JOIN Produit ON se_compose_prepa.IDproduit = Produit.IDproduit
+  WHERE Produit.NomProduit = ingredient
+);
 
 END |
 DELIMITER ;
@@ -21,3 +25,5 @@ DELIMITER ;
 -----------------------------------------------
 -- Commande de test
 -----------------------------------------------
+
+CALL preparation_ingredient('Sardonyx');
