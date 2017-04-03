81fd313e2a425df5c867f08e69cdeb432cc9da9e
AlexandreLec <alexandre.lecomte@viacesi.fr>
2017-04-03 10:04:05 +0200

    ajouter recette
    version finale opérationnelle

--- requetes_sql/ajouter_recette.sql
+++ requetes_sql/ajouter_recette.sql
@@ -11,33 +11,47 @@ CREATE PROCEDURE ajouter_recette(
   IN nom char(25),
   IN temperature int(11),
   IN diluant varchar(25),
-  IN IDingredient1 int(11),
+  IN ingredient1 varchar(255),
   IN qte1 int(11),
   IN fraichMin1 int(11),
   IN fraichMax1 int(11),
   IN temps1 int(11),
-  IN IDingredient2 int(11),
+  IN ingredient2 varchar(255),
   IN qte2 int(11),
   IN fraichMin2 int(11),
   IN fraichMax2 int(11),
   IN temps2 int(11),
-  IN IDingredient3 int(11),
+  IN ingredient3 varchar(255),
   IN qte3 int(11),
   IN fraichMin3 int(11),
   IN fraichMax3 int(11),
   IN temps3 int(11),
-  IN IDinventeur int(11)
+  IN inventeur varchar(255)
 )
 BEGIN
 INSERT INTO Proposition(`Nom`,`Temperature`,`Diluant`,`IDPersonne`)
-VALUES (nom,temperature,diluant,IDinventeur);
+SELECT nom,temperature,diluant,Personne.IDPersonne
+FROM Personne
+WHERE Personne.Nom = inventeur;
+
 SELECT MAX(IDProposition) INTO @IDPropo
 FROM Proposition;
+
 INSERT INTO se_compose_propo(`QteIngredient`,`FraicheurMin`,`FraicheurMax`,`Temps`,`IDProposition`,`IDproduit`)
-VALUES
-(qte1, fraichMin1,fraichMax1,temps1,@IDPropo,IDingredient1),
-(qte2, fraichMin2,fraichMax2,temps2,@IDPropo,IDingredient2),
-(qte3, fraichMin3,fraichMax3,temps3,@IDPropo,IDingredient3);
+SELECT qte1, fraichMin1,fraichMax1,temps1,@IDPropo,IDproduit
+FROM Produit
+WHERE NomProduit = ingredient1;
+
+INSERT INTO se_compose_propo(`QteIngredient`,`FraicheurMin`,`FraicheurMax`,`Temps`,`IDProposition`,`IDproduit`)
+SELECT qte2, fraichMin2,fraichMax2,temps2,@IDPropo,IDproduit
+FROM Produit
+WHERE NomProduit = ingredient2;
+
+INSERT INTO se_compose_propo(`QteIngredient`,`FraicheurMin`,`FraicheurMax`,`Temps`,`IDProposition`,`IDproduit`)
+SELECT qte3, fraichMin3,fraichMax3,temps3,@IDPropo,IDproduit
+FROM Produit
+WHERE NomProduit = ingredient3;
+
 END |
 DELIMITER ;
 
@@ -45,4 +59,4 @@ DELIMITER ;
 -- Commande de test
 ----------------------------------------------- 
 
-CALL ajouter_recette('Potion de mort',100,'Vin',35,5,10,5,10,36,10,5,1,5,68,1,5,10,2,20);
+CALL ajouter_recette('Potion de test',100,'Vin','Sardonyx',5,10,5,10,'Azurite',10,5,1,5,'Zircon',1,5,10,2,'Merlin');
