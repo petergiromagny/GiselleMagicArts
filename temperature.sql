5d5734fb5e22697e3ee8f6afaa0998619b265be2
Aurélien Klein <aurelien.klein@viacesi.fr>
2017-04-03 13:59:57 +0200

    Delete temperature.sql

--- temperature.sql
+++
@@ -1,4 +0,0 @@
-SELECT Preparation.Temperature, Produit.NomProduit
-FROM (Preparation INNER JOIN Produit ON Preparation.IDproduit = Produit.IDproduit)
-WHERE Preparation.Temperature > 0
-ORDER BY Temperature ASC;
