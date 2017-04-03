856c6e36c00d6919b005f9a0616e1e76d346fa3a
pierre-geeraert <pierre.geeraert@viacesi.fr>
2017-04-03 23:51:35 +0200

    add explaination of affichage_stocks

--- requetes_sql/affichage-stock.sql
+++ requetes_sql/affichage-stock.sql
@@ -1,22 +1,25 @@
-ELIMITER | -- On change le délimiteur
-CREATE PROCEDURE affichage_stocks(in NomIngredient_add char(30), in nbr_add int)    
+DELIMITER | -- On change le délimiteur
+CREATE PROCEDURE affichage_stocks(in NomIngredient_add char(30), in nbr_add int,in Fraicheur_add int)    
     
 BEGIN
-    	
-    select distinct 'avant',NomProduit, NbStock
+    
+    select 'avant',NomProduit, NbStock,Fraicheur
     from Stock,Ingredient,Produit
-    where Stock.IDproduit = Produit.IDproduit and Produit.NomProduit=NomIngredient_add;
+    where Stock.IDproduit = Ingredient.IDproduit and Ingredient.IDproduit = Produit.IDproduit and Produit.NomProduit=NomIngredient_add;
 
 
     update Stock 
-    join Produit on Stock.Idproduit=Produit.IDproduit and NomProduit=NomIngredient_add 
+    join Produit on Stock.Idproduit=Produit.IDproduit and NomProduit=NomIngredient_add and Stock.Fraicheur=fraicheur_add 
     set NbStock=NbStock+nbr_add;
 
-	
 
-    select distinct 'aprés',NomProduit, NbStock
+
+    select 'aprés',NomProduit, NbStock,Fraicheur
     from Stock,Ingredient,Produit
     where Stock.IDproduit = Ingredient.IDproduit and Ingredient.IDproduit = Produit.IDproduit and Produit.NomProduit=NomIngredient_add;
+    
+    select distinct NomProduit,NbStock,Fraicheur from Produit,Stock where Produit.IDproduit=Stock.IDproduit;
   
-END|   
+END  
 Delimiter ;
+
--- requetes_sql/potion_fct_diluant.sql
+++ requetes_sql/potion_fct_diluant.sql
@@ -1,9 +1,10 @@
 delimiter |
-create procedure diluant(in NomDiluant char(30) )
+create procedure diluant(in NomDiluant_add char(30) )
 begin
 select Produit.NomProduit,Diluant.NomDiluant  
 from Produit,Preparation,Diluant 
-where Produit.IDproduit=Preparation.IDproduit and Preparation.IDdiluant=Diluant.IDdiluant and Diluant.NomDiluant=NomDiluant;
+where Produit.IDproduit=Preparation.IDproduit and Preparation.IDdiluant=Diluant.IDdiluant and Diluant.NomDiluant=NomDiluant_add;
 
 end|
 delimiter ;
+
