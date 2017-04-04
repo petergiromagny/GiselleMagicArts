361b3eb08060261ea4add821c3e9fb9dbf361387
AlexandreLec <alexandre.lecomte@viacesi.fr>
2017-04-04 10:49:59 +0200

    marge giselle

--- requetes_sql/margeGiselle.sql
+++ requetes_sql/margeGiselle.sql
@@ -11,15 +11,17 @@ DELIMITER |
 CREATE PROCEDURE marge_preparation()
 BEGIN
 
-SELECT req.IDproduit, req.NomProduit, (req.Prix-SUM((se_compose_prepa.QteIngredient*Produit.Prix))) AS 'Marge'
+SELECT req.IDproduit, req.NomProduit, (req.Prix-SUM((se_compose_prepa.QteIngredient*Produit.Prix))-req.PrixDiluant) AS 'Marge'
 FROM se_compose_prepa
 INNER JOIN Produit
 ON se_compose_prepa.IDproduit = Produit.IDproduit
 INNER JOIN (
-  SELECT Preparation.IDproduit, Produit.NomProduit, Produit.Prix
+  SELECT Preparation.IDproduit, Produit.NomProduit, Produit.Prix, Diluant.PrixDiluant
   FROM Produit
   INNER JOIN Preparation
   ON Produit.IDproduit = Preparation.IDproduit
+  INNER JOIN Diluant
+  ON Preparation.IDDiluant = Diluant.IDDiluant
 ) AS req
 ON req.IDproduit = se_compose_prepa.IDproduitFinal
 GROUP BY (req.IDproduit);
