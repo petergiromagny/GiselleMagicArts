848c84ddfc3a675ff4469b1bd17afbf4d5a1ea9e
Aurélien Klein <aurelien.klein@viacesi.fr>
2017-04-04 11:59:34 +0200

    Add files via upload

--- vue_proposition_personne.sql
+++ vue_proposition_personne.sql
@@ -1,1 +1,4 @@
 CREATE VIEW Proposition_personne
+(ID_proposition, Nom, Proposition)
+AS SELECT Proposition.IDProposition, Personne.Nom, Proposition.Nom
+FROM (Proposition INNER JOIN Personne ON Proposition.IDPersonne = Personne.IDPersonne);
