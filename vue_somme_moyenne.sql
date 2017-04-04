29cd15f1b4357163ff6486fe3214cb5ca1f8df5e
Aurélien Klein <aurelien.klein@viacesi.fr>
2017-04-04 10:41:41 +0200

    Add files via upload

--- vue_somme_moyenne.sql
+++ vue_somme_moyenne.sql
@@ -1,11 +1,11 @@
-CREATE VIEW Somme
+CREATE VIEW Somme_ingredient_potion
 (Produit, Valeur)
 AS SELECT Preparation.IDproduit, SUM(se_compose_prepa.QteIngredient)
 FROM (Preparation INNER JOIN se_compose_prepa ON Preparation.IDproduit = se_compose_prepa.IDproduitFinal)
 WHERE Temperature > 0
 GROUP BY IDproduit;
 
-CREATE VIEW Moyenne
+CREATE VIEW Moyenne_ingredient_potion
 (Produit, Valeur)
 AS SELECT AVG(Valeur)
 FROM Somme;
